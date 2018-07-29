807ab14fe0157d68dd682182e55791b6b982a666
MR-zn <846179626@qq.com>
2018-07-29 15:21:16 +0800

    able to setting icon-position

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- <img src="./assets/logo.png"> -->
     <router-view></router-view>
   </div>
 </template>
@@ -11,6 +11,7 @@ export default {
 }
 </script>
 
+<!--
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -21,3 +22,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+-->
--- src/components/button.vue
+++ src/components/button.vue
@@ -1,18 +1,28 @@
 <template>
-    <button class="g-button" v-bind:class="`${icon}`">
-        <slot></slot>
+    <button class="g-button" v-bind:class="[`${icon}`,{[`icon-${iconPosition}`]: true}]">
+        <p>
+            <slot></slot>
+        </p>
     </button>
 </template>
 
 <script>
 export default {
-    props: ['icon']
-};
+    props: {
+        icon: {},
+        iconPosition: {
+            type: String,
+            default: 'left',
+            // validator (value) {
+            //     console.log(value);
+            // }
+        }
+    }
+}
 </script>
 
 
 <style>
-
 @import url(https://use.fontawesome.com/releases/v5.2.0/css/all.css);
 
 .g-button {
@@ -24,6 +34,29 @@ export default {
     background: #fff;
     transition: all 0.2s;
     cursor: pointer;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+}
+
+.fa-sliders-h:before {
+    order: 1;
+    margin-right: 0.1em;
+}
+
+.g-button p {
+    order: 2;
+}
+
+.icon-right:before {
+    order: 2;
+    margin-right: 0;
+    margin-left: 0.1em;
+}
+
+.icon-right p {
+    order: 1;
 }
 
 .g-button:hover {
